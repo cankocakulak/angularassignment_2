7c8e84474c9de61e2001ad6e9475df5a73cd4c4b
Can <mcank2000@gmail.com>
2024-07-16 15:18:44 +0300

    backend and frontend overall setup

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/order', (req, res) => {
+    const order = [
+        { id: 1, name: 'Cool Shirt', price: 25.0, qty: 3, weight: 0.5 },
+        { id: 2, name: 'Cool Pants', price: 45.0, qty: 2, weight: 1 },
+        { id: 3, name: 'Light Saber', price: 125.0, qty: 1, weight: 5 }
+    ];
+    res.json({ order });
+});
+
+app.get('/shipping', (req, res) => {
+    const totalWeight = req.query.weight;
+    const shipping = {
+        carrier: 'UPS',
+        address: {
+            name: 'Amanda Miller',
+            phone: '555-555-5555',
+            address_line1: '525 S Winchester Blvd',
+            city_locality: 'San Jose',
+            state_province: 'CA',
+            postal_code: '95128',
+            country_code: 'US'
+        },
+        cost: 7.99
+    };
+    res.json({ shipping });
+});
+
+app.get('/tax', (req, res) => {
+    const tax = {
+        amount: 0.07
+    };
+    res.json({ tax });
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
